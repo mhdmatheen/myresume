abfef98aa99f169ffb399a07f18f1b09dade18eb
Matheen <matheen@finobi.app>
2025-06-15 14:09:08 +0530

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Taskbar from "./shell/components/taskbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +26,9 @@ export default function RootLayout({
     <html lang="en" className="light" data-theme="light">
       <head>
         <meta name="color-scheme" content="light only" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&amp;display=swap" rel="stylesheet"></link>
